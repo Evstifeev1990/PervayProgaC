cc95977bda5ccb820014ac6c50ec24aaaeb243f4
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-07-13 06:13:35 +0300

    Добавили 006

---
+++ one_p006/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 1;
+int b = 6;
+int c = 2;
+int d = 4;
+int e = 8;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("Максимальное число - ");
+Console.WriteLine(max);

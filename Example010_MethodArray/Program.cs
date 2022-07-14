946a7b251b007d7af08701bb1ad5e4614a98a84b
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-07-14 19:01:30 +0300

    Add Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 12, 23, 35, 42, 53, 35, 72, 85, 96 };
+
+int n = array.Length;
+
+int fide = 35;
+
+int index = 0;
+
+while(index<n)
+{
+    if(array[index] == fide)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+
+}

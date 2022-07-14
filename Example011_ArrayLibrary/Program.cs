0c7af562633358661d9f60dc7799d4ce86b903fc
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-07-14 20:29:20 +0300

    Add Example011_ArrayLibrary

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,30 @@
+﻿void FillArray (int[] collection)
+{
+    int lenght = collection.Length;
+
+    int index = 0;
+
+    while(index<lenght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray (int[] col)
+{
+    int len = col.Length;
+
+    int possishion = 0;
+
+    while(possishion<len)
+    {
+        Console.WriteLine(col[possishion]);
+        possishion++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);

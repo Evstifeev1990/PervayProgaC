77da66e114fa235d819285e9b943c516098215d2
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-07-19 09:14:42 +0300

    Add Examle012_For

---
+++ Example012_For/Program.cs
@@ -0,0 +1,40 @@
+﻿//Вид 4 методов (что-то принимают, что-то возвращают)
+
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = ""; // = string.Empty;
+    
+//     while(i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+
+// }
+
+string Method4(int count, string text)
+{
+    string result = ""; // = string.Empty;
+    
+    for(int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+
+}
+
+string res = Method4(4, "Текст");
+Console.WriteLine(res);
+
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.WriteLine($"{i}x{j}={i*j}");
+    }
+    Console.WriteLine();
+}
